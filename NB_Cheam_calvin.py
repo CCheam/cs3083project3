25a9b0f3f9f9ca33f32216a83f55ec5621d65a2b
CCheam <ccheam25@gmail.com>
2026-03-11 11:35:29 -0500

    setup main and start on conf matrix

--- NB_Cheam_calvin.py
+++ NB_Cheam_calvin.py
@@ -7,3 +7,14 @@ import nltk
 import re 
 import math 
 
+def generate_confusion_matrix():
+    mtrx = []
+    return mtrx
+def main():
+
+    conf = generate_confusion_matrix()
+    for row in conf:
+        print(row)
+    return -1
+if __name__ == "__main__":
+    main()

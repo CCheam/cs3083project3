77f947b53d5e489abb938ec1519ace49d454e82f
CCheam <ccheam25@gmail.com>
2026-03-23 11:40:02 -0500

    fixed tokenization to work and adjusted so periods are filtered too

--- NB_Cheam_calvin.py
+++ NB_Cheam_calvin.py
@@ -12,12 +12,12 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 nltk.download('stopwords')
 nltk.download('punkt')
-
+nltk.download('punkt_tab')
 def text_tokenization(text):
     #imports stopwords, sets and tokenizes text. 
     #Filters by comparing the tokens to stopwords list
-
     stop_words = set(stopwords.words('english'))
+    stop_words.add('.')
     tokens = word_tokenize(text.lower())
     filtered_tokens = [word for word in tokens if word not in stop_words]
     #test prints to ensure tokenization works right
@@ -40,6 +40,7 @@ def main():
             raw_text = f.read()
     except FileNotFoundError:
         print(f"Error: The file '{file_path}' was not found.")
+    text_tokenization(raw_text)
 
 if __name__ == "__main__":
     main()

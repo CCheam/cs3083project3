431d2e961982dda0ae333ff3ca58b1b2dcf4f2db
CCheam <ccheam25@gmail.com>
2026-03-23 11:22:41 -0500

    adjusted intepretation bug

--- NB_Cheam_calvin.py
+++ NB_Cheam_calvin.py
@@ -7,9 +7,23 @@ import nltk
 import re 
 import math 
 
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+def normalize_text(text):
+    ft = text.lower()
+    for char in ft:
+        if not char.isalnum():
+            #remove
+            print()
+    return ft
+    
 def generate_confusion_matrix():
-    mtrx = []
+    mtrx = [[],[],[],[],[],[]]
     return mtrx
+
+def generate ():
+    return 1
 def main():
 
     conf = generate_confusion_matrix()
